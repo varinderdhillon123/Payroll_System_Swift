198491a62a2842e97d74ce2feae6a77118144406
Komal2006 <komaldeepkr99@gmail.com>
2019-10-16 19:48:43 -0400

    [komal] PartTime class - body of constructor

--- Payroll_System_Swift/PartTime.swift
+++ Payroll_System_Swift/PartTime.swift
@@ -13,6 +13,9 @@ class PartTime
     
     init(name: String, age: Int, earnings: Double, rate: Double, hoursWorked: Float)
     {
-        
+        self.rate = rate
+        self.hoursWorked = hoursWorked
+      // super.init(name: name, age: age, earnings: earnings)
+
     }
 }
