1682905af0576d9405a1f3f2e24bfa8aea8d5d37
camysandhu <fmsharmn@gmail.com>
2019-10-22 19:12:01 -0400

    printData body

--- Payroll_System_Swift/Car.swift
+++ Payroll_System_Swift/Car.swift
@@ -10,13 +10,16 @@ class Car: Vehicle
 {
   var distanceTravelled: Double
   var capacity: String
+    
     init(make: String, color: String, plate: String, distanceTravelled: Double, capacity: String)
     {
         self.distanceTravelled = distanceTravelled
         self.capacity = capacity
         super.init(make: make, color: color, plate: plate)
     }
-    override func printMyData() {
-        <#code#>
+    override func printMyData()
+    {
+        print("Distance: \(distanceTravelled)",
+            "Capacity: \(capacity)")
     }
 }
