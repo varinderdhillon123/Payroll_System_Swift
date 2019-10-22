dab4044462e9cee771f5d589ba16c7b7a272e0ba
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-10-22 18:59:54 -0400

    [charan] employee

--- Payroll_System_Swift/Employee.swift
+++ Payroll_System_Swift/Employee.swift
@@ -6,7 +6,8 @@
 //
 
 import Foundation
-class Employee{
+class Employee
+{
     var empID: Int
     var name: String
     var age: Int
@@ -16,16 +17,14 @@ class Employee{
         case FixedBasedPartTime
         case CommissionBasedPartTime
         case Intern
-        
     }
-    //var earnings: Double
     
-    init(empID: Int, name: String, age:Int, emptype: TypeofEmployee) {
+    init(empID: Int, name: String, age:Int, emptype: TypeofEmployee)
+    {
         self.empID = empID
         self.name = name
         self.age = age
         self.emptype = emptype
-        //self.earnings=earnings
     }
     
 }
