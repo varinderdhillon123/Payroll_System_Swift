26fec1c3b03b7c54d4cbcb0822193222e180d763
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-10-23 14:29:24 -0400

    creating object for fixedBased class

--- Payroll_System_Swift/main.swift
+++ Payroll_System_Swift/main.swift
@@ -31,8 +31,9 @@ func readJsonFileArray(jsonFileName: String)
                 if let emptype = jsonDictionay["emptype"] as? TypeofEmployee
         
                 {
-                    //print(emptype)
+                   
                     emp.emptype = emptype
+                     print(emptype)
                 }
                 if let name = jsonDictionay["name"] as? String
                 {
@@ -64,13 +65,13 @@ func readJsonFileArray(jsonFileName: String)
                let obj = Intern(empID: emp.empID!,name: emp.name!, age: emp.age!, emptype: emp.emptype!, schoolName: "Lambton", earnings: 100.0)
                    obj.printMyData()
             }
-//            else if emp.emptype! == .PartTime {   //"PartTime"
-//            {
-//                // print("\n\n")
-//                let PTobj = PartTime(empID: user.id!,PTEmptype: user.emptype!, PTEmpName: user.name!, PTEmpAge: user.age!, PTEmpEarning: 100.1, Rate: 23, HoursWorked: user.hourswork!)
-//                PTobj.printMyData()
-//
-//            }
+            else if emp.emptype! == .FixedBasedPartTime   //"PartTime"
+            {
+                // print("\n\n")
+                let PTobj = FixedBasedPartTime(empID: emp.empID!,name: emp.name!,  age: emp.age!, fixedAmount: 500.0, rate: 2, hoursWorked: 23, emptype: .FixedBasedPartTime)
+                PTobj.printMyData()
+
+            }
             
             
             
@@ -83,3 +84,7 @@ func readJsonFileArray(jsonFileName: String)
 
 
 readJsonFileArray(jsonFileName: "DataofEmp")
+
+var e1 = FixedBasedPartTime(empID: 1,name: "ABC", age:25, fixedAmount: 200.0, rate: 2.5, hoursWorked: 23, emptype: .FixedBasedPartTime)
+e1.printMyData()
+
