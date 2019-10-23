13c9a72ba526b0154220d5cdbb97383685931e1e
varinderdhillon123 <varinder701dhillon@gmail.com>
2019-10-23 16:08:50 -0400

    [varinder] extension float

---
+++ Payroll_System_Swift/extension+Float.swift
@@ -0,0 +1,16 @@
+//
+//  extension+Float.swift
+//  Payroll_System_Swift
+//
+//  Created by MacStudent on 2019-10-23.
+//
+
+import Foundation
+
+extension Float
+{
+    func addHours() -> String
+    {
+        return "\(self) hours"
+    }
+}
