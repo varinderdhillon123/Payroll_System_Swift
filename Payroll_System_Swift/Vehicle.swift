2901e6aeb2c555992efec8155c96aced3f3e9858
komal2006 <komaldeepkr99@gmail.com>
2019-10-24 13:44:10 -0400

    [komal] vehicle

--- Payroll_System_Swift/Vehicle.swift
+++ Payroll_System_Swift/Vehicle.swift
@@ -29,8 +29,8 @@ class Vehicle: IPrintable
     
     func addVehicle(v:Vehicle){
         vehicleDic.updateValue(v, forKey: v.plate)
-        
-    }
+      }
+   
     
     func printMyData()
     {
